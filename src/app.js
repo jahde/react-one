7f27a55f127414a2f912bc6dc7f4361a6b3b41d5
jahde <jahde@classpass.com>
2016-06-26 19:33:31 -0400

    add env variables

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const App = React.createClass({
   render: function() {
     return (
       <div className={styles.wrapper}>
+        <h1>Environment: {__NODE_ENV__}</h1>
         Text text text
       </div>
     );
